f1c2c98a5d7d474bd0b444aff01deb014ce12582
Maynewi <affi.amin.work@gmail.com>
2023-08-15 18:02:05 +0100

    first steps in ES6

---
+++ index.js
@@ -0,0 +1,10 @@
+let essm = "amine"
+let lakab = "affi"
+
+//ki yabda star wehd m console ma tgalgsh b syntax l aady
+//snn twally tesstaaml e syntax hedha w t indenti kima theb
+let essmek = `${essm} ${lakab}`
+
+console.log(essmek)
+
+document.getElementById("mathalan").innerText = essmek
