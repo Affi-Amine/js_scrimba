af03afadee0ba8000b05612020ace349736ee70a
Maynewi <affi.amin.work@gmail.com>
2023-08-16 21:35:49 +0100

    understanding promises

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-export const data = [1,2,3]
+const userData = {
+    name: "amine",
+    age: 20
+  };
+  
+  const returnData = (name) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const user = userData.name;
+        if (user) {
+            //e resolve bsh testaamlha fl .then
+          resolve(userData);
+        } else {
+          reject("user not found");
+        }
+      }, 3000);
+    });
+  };
+  
+  returnData("amine")
+    .then((userData) => {
+      console.log("user data", userData);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  
